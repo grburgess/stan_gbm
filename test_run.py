6f9125112971b7088808a0b180c416996133372b
grburgess <jmichaelburgess@gmail.com>
2019-01-22 21:16:31 +0100

    add test run

---
+++ test_run.py
@@ -0,0 +1,31 @@
+import stan_utility
+import pystan
+import numpy as np
+model = stan_utility.compile_model('test.stan', model_name='test_gbm')
+
+data = pystan.read_rdump('test_data.R')
+
+N_gen_spectra = 100
+model_energy = np.logspace(0,5,N_gen_spectra)
+data['N_gen_spectra'] = N_gen_spectra
+data['model_energy'] = model_energy
+
+warmup = 1000
+iter = 100
+
+total = warmup + iter
+
+chains = 4
+
+fit = model.sampling(
+    data=data,
+    iter=total,
+    warmup=warmup,
+    chains=chains,
+    n_jobs=chains,
+    control=dict(max_treedepth=13,
+    #             adapt_delta=0.9
+    ),
+    seed=1234)
+
+stan_utility.stanfit_to_hdf5(fit, '/data/jburgess/stan_fits/gbm_stan_fit.h5')
