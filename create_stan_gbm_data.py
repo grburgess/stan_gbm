feb2d37fb74466d791b0841d9f2aea01dfb4c166
grburgess <jmichaelburgess@gmail.com>
2019-01-26 21:59:36 +0100

    updates to data creator

--- create_stan_gbm_data.py
+++ create_stan_gbm_data.py
@@ -1,6 +1,8 @@
-from glob import glob
+from astropy.cosmology import WMAP9 as cosmo
 from threeML import *
 import pystan
+from glob import glob
+import pandas as pd
 
 
 def get_tr_datalists(grb,pha):
@@ -16,7 +18,7 @@ def get_tr_datalists(grb,pha):
         n_bins = int(f[1].header['NAXIS2'])
     
     
-    
+     
     
     datalists = []
 
@@ -77,15 +79,20 @@ pha_files = [glob(os.path.join(p,'*.pha') ) for p in single_pulse]
 
 
 all_data_sets = []
+grb_name = []
+grb_number = []
 
+for n, pha in enumerate(pha_files[:4]):
 
-for pha in pha_files:
+    grb = pha[0].split('/')[0]
 
-    grb = pha[0].split('/')[0
-
+    
     
     this_data_set=get_tr_datalists(grb,pha=pha)
 
+    grb_name.extend([grb]*len(this_data_set))
+    grb_number.extend([n+1]*len(this_data_set))
+    
     all_data_sets.extend(this_data_set)
 
 
@@ -93,8 +100,8 @@ for pha in pha_files:
 data_dict = {}
 
 max_n_dets = max([ len(ds) for ds in all_data_sets])
-max_n_chans = np.max( [ [ len(d.observed_counts)  for d in  ds] for ds in all_data_sets])
-max_n_echans = np.max( [ [ d.response.matrix.shape[1]  for d in  ds] for ds in all_data_sets])
+max_n_chans = max(np.max( [ [ len(d.observed_counts)  for d in  ds] for ds in all_data_sets]))
+max_n_echans = max(np.max( [ [ d.response.matrix.shape[1]  for d in  ds] for ds in all_data_sets]))
 
 n_intervals = len(all_data_sets)
 
@@ -118,16 +125,36 @@ n_echan = np.zeros((n_intervals,max_n_dets))
 n_chan = np.zeros((n_intervals,max_n_dets))
 n_dets = [ len(ds) for ds in all_data_sets]
 
+
+
 masks = np.zeros((n_intervals,max_n_dets,max_n_chans))
 n_channels_used = np.zeros((n_intervals,max_n_dets))
+grb_id = np.zeros(n_intervals)
 ebounds_lo = np.zeros((n_intervals, max_n_dets, max_n_echans))
 ebounds_hi = np.zeros((n_intervals, max_n_dets, max_n_echans))
+
+cbounds_lo = np.zeros((n_intervals, max_n_dets, max_n_chans))
+cbounds_hi = np.zeros((n_intervals, max_n_dets, max_n_chans))
+
+
 dl = np.zeros(n_intervals)
 z = np.zeros(n_intervals)
 
+redshifts = pd.read_json('redshifts.json')
+
+
+
+total_number_of_channels_used = 0
+grb_counter = 0
+
+print(len(grb_number))
+
 for i,ds in enumerate(all_data_sets):
+
+    z_idx = redshifts['trigger'] == grb_name[i] 
+
     
-    z[i] =2.332
+    z[i] = float(redshifts['z'][z_idx])
     dl[i] = cosmo.luminosity_distance(z[i]).to('cm').value
     
     
@@ -162,9 +189,17 @@ for i,ds in enumerate(all_data_sets):
         n_echan[i,j] = aa.response.matrix.shape[1]
         ebounds_lo[i,j,:aa.response.matrix.shape[1]] = aa.response.monte_carlo_energies[:-1]
         ebounds_hi[i,j,:aa.response.matrix.shape[1]] = aa.response.monte_carlo_energies[1:]
+        cbounds_lo[i,j,:aa.response.matrix.shape[0]] = aa.response.ebounds[:-1]
+        cbounds_hi[i,j,:aa.response.matrix.shape[0]] = aa.response.ebounds[1:]
         exposures[i,j] = aa.exposure
         
+        total_number_of_channels_used += sum(aa.mask)
+
+    
+    
         
+data_dict['N_all'] = int(total_number_of_channels_used)
+data_dict['N_grbs'] = len(np.unique(grb_number))
 data_dict['max_n_echan'] = max_n_echans
 data_dict['max_n_chan'] = max_n_chans
 data_dict['N_dets'] = n_dets
@@ -176,16 +211,19 @@ data_dict['idx_background_nonzero'] = idx_background_nonzero.astype(int)
 data_dict['idx_background_zero'] = idx_background_zero.astype(int)
 data_dict['N_bkg_zero'] = n_bkg_zero.astype(int)
 data_dict['N_bkg_nonzero'] = n_bkg_nonzero.astype(int)
-
+data_dict['object_idx'] = np.array(grb_number).astype(int)
 data_dict['background_errors'] = background_errors
 data_dict['ebounds_lo'] = ebounds_lo
 data_dict['ebounds_hi'] = ebounds_hi
+data_dict['cbounds_lo'] = cbounds_lo
+data_dict['cbounds_hi'] = cbounds_hi
 data_dict['exposure'] = exposures
 data_dict['response'] = responses
 data_dict['mask'] = masks.astype(int)
 data_dict['N_channels_used'] = n_channels_used.astype(int)
+data_dict['grb_id'] = np.array(grb_number).astype(int)
 data_dict['dl'] = dl
 data_dict['z'] = z
 
 
-pystan.stan_rdump(data_dict,'test_data.R')
+pystan.stan_rdump(data_dict,'all_data.R')
